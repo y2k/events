46b7f78883186bd944f10c3dc976db67bf2d4c51
y2k <itwisterlx@gmail.com>
2026-04-21 20:58:58 +0200

    Inline request handler in worker export

--- src/main.clj
+++ src/main.clj
@@ -3,13 +3,11 @@
             [fetch :as fetch]
             [telegram :as tg]))
 
-(defn- handle-request [request env ctx]
-  (fetch/with-fetch js/fetch
-    (fn []
-      (tg/with-config {:token env.TELEGRAM_BOT_TOKEN
-                       :chat_id env.TELEGRAM_CHAT_ID}
-        (fn []
-          (handler/handle-request request env ctx))))))
-
 (export-default
- {:fetch handle-request})
+ {:fetch (fn [request env ctx]
+           (fetch/with-fetch js/fetch
+             (fn []
+               (tg/with-config {:token env.TELEGRAM_BOT_TOKEN
+                                :chat_id env.TELEGRAM_CHAT_ID}
+                 (fn []
+                   (handler/handle-request request env ctx))))))})
