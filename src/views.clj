7ba2e5bc45710bf6f1811f461ae338ba401a366c
y2k <itwisterlx@gmail.com>
2026-04-21 17:35:03 +0200

    Make layout title configurable

--- src/views.clj
+++ src/views.clj
@@ -1,29 +1,29 @@
 (ns views)
 
-(defn- layout [content]
+(defn- layout [title content]
   [:html {}
    [:head {}
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-    [:title {} "Рекомендовать событие"]
+    [:title {} title]
     [:link {:rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"}]]
    [:body {}
     [:main {:class "container"}
-     content]]])
+      content]]])
 
 (defn home-page []
-  (layout
+  (layout "Рекомендовать событие"
    [:form {:method "POST" :action "/submit"}
     [:fieldset {}
      [:label {} "Ссылка на событие (link to event)"
       [:input {:name "link_to_event"
-               :placeholder "https://"
-               :type "url"
-               :required true}]]]
+                :placeholder "https://"
+                :type "url"
+                :required true}]]]
     [:input {:type "submit" :value "Советую сходить!"}]]))
 
 (defn submit-result [link]
-  (layout
+  (layout "Рекомендовать событие"
    [:article {}
     [:p {} "Спасибо! Ссылка получена: " link]
     [:a {:href "/"} "Вернуться"]]))
